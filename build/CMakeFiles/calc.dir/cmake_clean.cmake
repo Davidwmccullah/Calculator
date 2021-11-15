e2e5bff904add78d5db307d67401d69334791be9
David McCullah <davidwmccullah@gmail.com>
2021-11-15 02:54:50 -0600

    Updated README.md.

---
+++ build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.dir/src/calculator.c.o"
+  "CMakeFiles/calc.dir/src/calculator.c.o.d"
+  "libcalc.pdb"
+  "libcalc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
